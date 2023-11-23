26a126f3cc848067d2109f08e3ee442d2856c9f4
Ivan Gulin <Ivan.Gulin@norbit.ru>
2023-11-23 20:41:48 +0300

    Добавлен WorkoutManagementSystemContext

---
+++ WorkoutManagementSystem.Svc/Infrastracture/WorkoutManagementSystemContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkoutManagementSystem.Svc.Infrastracture
+{
+    public class WorkoutManagementSystemContext : DbContext
+    {
+        public WorkoutManagementSystemContext(DbContextOptions<WorkoutManagementSystemContext> options) : base(options) 
+        {
+            
+        }
+    }
+}
--- WorkoutManagementSystem/Program.cs
+++ WorkoutManagementSystem/Program.cs
@@ -1,4 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using WorkoutManagementSystem.Svc.Infrastracture;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<WorkoutManagementSystemContext>(options => options.UseNpgsql(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
