e4e1835f45e9a3699329ac6dda95ae0b2bcbdc6d
Ivan Gulin <Ivan.Gulin@norbit.ru>
2023-11-26 13:18:27 +0300

    Добавлена сущность StarParticipants, создана миграция

--- WorkoutManagementSystem.Svc/Infrastracture/Entities/Workout.cs
+++ WorkoutManagementSystem.Svc/Infrastracture/Entities/Workout.cs
@@ -5,5 +5,6 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public DateTime DateCreation { get; set; }
+        public StarParticipants? StarParticipants { get; set; }
     }
 }
---
+++ WorkoutManagementSystem.Svc/Migrations/20231126101616_CreateStarParticipantsTable.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace WorkoutManagementSystem.Svc.Migrations
+{
+    public partial class CreateStarParticipantsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "StarParticipantsId",
+                table: "Workouts",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "StarParticipants",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Achievements = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StarParticipants", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Workouts_StarParticipantsId",
+                table: "Workouts",
+                column: "StarParticipantsId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Workouts_StarParticipants_StarParticipantsId",
+                table: "Workouts",
+                column: "StarParticipantsId",
+                principalTable: "StarParticipants",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Workouts_StarParticipants_StarParticipantsId",
+                table: "Workouts");
+
+            migrationBuilder.DropTable(
+                name: "StarParticipants");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Workouts_StarParticipantsId",
+                table: "Workouts");
+
+            migrationBuilder.DropColumn(
+                name: "StarParticipantsId",
+                table: "Workouts");
+        }
+    }
+}
