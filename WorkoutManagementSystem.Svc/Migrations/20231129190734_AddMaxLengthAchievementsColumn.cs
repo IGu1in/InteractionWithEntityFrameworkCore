1338e471fd274cbe61d1a199173659e08497df78
Ivan Gulin <Ivan.Gulin@norbit.ru>
2023-11-29 22:08:02 +0300

    Добавлена максимальная длина для Achievements

---
+++ WorkoutManagementSystem.Svc/Migrations/20231129190734_AddMaxLengthAchievementsColumn.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorkoutManagementSystem.Svc.Migrations
+{
+    public partial class AddMaxLengthAchievementsColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Achievements",
+                table: "StarParticipants",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Achievements",
+                table: "StarParticipants",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(200)",
+                oldMaxLength: 200);
+        }
+    }
+}
